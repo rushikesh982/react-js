c8e917e95a553c743373199dd88c69521b54ecdb
rushikesh garad <garadrushikesh042@gmail.com>
2025-01-11 11:11:31 +0530

    multiline code methods

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 function App(){
   return(
-    <h1>hello react js</h1>
+    // to write multiple line code get first any xml parent and write code in that
+    // first method
+    <div>
+      <h1>hello react js</h1>
+      <h1>hello react js</h1>
+    </div>
   )
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
