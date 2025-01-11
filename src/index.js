fee3910755b0eb09f944371715e249805a14718b
rushikesh garad <garadrushikesh042@gmail.com>
2025-01-11 15:23:32 +0530

    html tags

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-let ab = "a2z";
-let bc = "it hub";
+let ab = "html technology";
+// let bc = "it hub";
 function App(){
   return(
     <>
     {/* template literals */}
       <h1>welcome {ab}</h1>
-      <h1>welcome{` ${ab} ${bc}`}</h1>
+      <p>this is p tag</p>
+      <pre>this is pre tag</pre>
+      <h1>h1 tag</h1>
+      <h2>h2 tag</h2>
+      <h3>h3 tag</h3>
+      <h4>h4 tag</h4>
+      <h5>h5 tag</h5>
+      <h6>h6 tag</h6>
+      <b>bold tag</b><br/>
+      <a>anchor tag</a>
+      {/* <h1>welcome{` ${ab} ${bc}`}</h1> */}
     </>
   )
 }
