6e5911c21e85bf3748f667cfefce41d7986fb366
rushikesh garad <garadrushikesh042@gmail.com>
2025-01-11 15:28:19 +0530

    html attributes

--- src/index.js
+++ src/index.js
@@ -5,19 +5,11 @@ let ab = "html technology";
 function App(){
   return(
     <>
-    {/* template literals */}
-      <h1>welcome {ab}</h1>
-      <p>this is p tag</p>
-      <pre>this is pre tag</pre>
-      <h1>h1 tag</h1>
-      <h2>h2 tag</h2>
-      <h3>h3 tag</h3>
-      <h4>h4 tag</h4>
-      <h5>h5 tag</h5>
-      <h6>h6 tag</h6>
-      <b>bold tag</b><br/>
-      <a>anchor tag</a>
-      {/* <h1>welcome{` ${ab} ${bc}`}</h1> */}
+      {/* attributes */}
+      {/* <div style={}></div> */}
+      <div className=''></div>
+      <img />
+      <img src='logo192.png '/>
     </>
   )
 }
